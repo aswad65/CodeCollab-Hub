14670203a9d637b308f9f68e6c1bdf893665b1ae
aswad65 <aswadraxa65@gmail.com>
2026-01-30 22:05:53 +0500

    first commit

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,12 @@ app.use(express.json({
     }
   }
 }));
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userrouter);
 app.use("/api/project", projectrouter);
